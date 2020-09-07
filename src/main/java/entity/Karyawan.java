5ca9265692da1ebb824e44fe23b765544cd6fbeb
Syarif <syarif.nurhid@gmail.com>
2020-09-07 15:05:59 +0700

    membuat entity class Karyawan beserta atributnya

---
+++ src/main/java/entity/Karyawan.java
@@ -0,0 +1,49 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Karyawan {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Integer id;
+	@Column
+	private String nama;
+	@Column
+	private String alamat;
+	@Column
+	private String posisi;
+	
+	
+	public Karyawan() {
+		
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getAlamat() {
+		return alamat;
+	}
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+	public String getPosisi() {
+		return posisi;
+	}
+	public void setPosisi(String posisi) {
+		this.posisi = posisi;
+	}
+}
