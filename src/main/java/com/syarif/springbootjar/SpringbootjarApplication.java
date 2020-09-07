dfff511db2b8185018735fff0c328ee55011845a
Syarif <syarif.nurhid@gmail.com>
2020-09-07 13:54:17 +0700

    menambahkan print hello world

--- src/main/java/com/syarif/springbootjar/SpringbootjarApplication.java
+++ src/main/java/com/syarif/springbootjar/SpringbootjarApplication.java
@@ -8,7 +8,7 @@ public class SpringbootjarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootjarApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Hello World");
 	}
 
 }
