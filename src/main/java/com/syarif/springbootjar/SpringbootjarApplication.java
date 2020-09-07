ac52d5e23dadd72b2cfe7fe648551e0c037e0a43
Syarif <syarif.nurhid@gmail.com>
2020-09-07 14:18:58 +0700

    menambahkan print belajar rest api dengan springboot

--- src/main/java/com/syarif/springbootjar/SpringbootjarApplication.java
+++ src/main/java/com/syarif/springbootjar/SpringbootjarApplication.java
@@ -10,6 +10,7 @@ public class SpringbootjarApplication {
 		SpringApplication.run(SpringbootjarApplication.class, args);
 		System.out.println("Hello World");
 		System.out.println("Nama saya Syarif");
+		System.out.println("Saya sedang belajar Rest API dengan Springboot");
 	}
 
 }
