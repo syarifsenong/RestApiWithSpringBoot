080327180ddb6767e74fcb49339d1784b4f1883f
Syarif <syarif.nurhid@gmail.com>
2020-09-07 19:53:12 +0700

    edit file main.java dan karyawan.java

--- src/main/java/entity/Karyawan.java
+++ src/main/java/entity/Karyawan.java
@@ -2,13 +2,17 @@ package entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @Entity
 @Table
+@EntityListeners(AuditingEntityListener.class)
 public class Karyawan {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
