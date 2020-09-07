9b97da0e36f3c083cda37bade2b28983487661e5
Syarif <syarif.nurhid@gmail.com>
2020-09-07 15:47:21 +0700

    menambahkan anotasi @Entity dan @Table di file Karyawan.java

--- src/main/java/entity/Karyawan.java
+++ src/main/java/entity/Karyawan.java
@@ -1,10 +1,14 @@
 package entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table
 public class Karyawan {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,33 +20,39 @@ public class Karyawan {
 	private String alamat;
 	@Column
 	private String posisi;
-	
-	
+
 	public Karyawan() {
-		
+
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNama() {
 		return nama;
 	}
+
 	public void setNama(String nama) {
 		this.nama = nama;
 	}
+
 	public String getAlamat() {
 		return alamat;
 	}
+
 	public void setAlamat(String alamat) {
 		this.alamat = alamat;
 	}
+
 	public String getPosisi() {
 		return posisi;
 	}
+
 	public void setPosisi(String posisi) {
 		this.posisi = posisi;
 	}
