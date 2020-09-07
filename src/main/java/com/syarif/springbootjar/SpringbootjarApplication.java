e8a06034a56411d64f1adc4e49de0222b89e7726
Syarif <syarif.nurhid@gmail.com>
2020-09-07 13:52:33 +0700

    file springboot

---
+++ src/main/java/com/syarif/springbootjar/SpringbootjarApplication.java
@@ -0,0 +1,14 @@
+package com.syarif.springbootjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjarApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/syarif/springbootjar/SpringbootjarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syarif.springbootjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
